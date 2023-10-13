a9aa8c68c69d961d3d131a8b238f87d521e1cbc7
Ishita928 <Canada@1964@@>
2023-10-13 22:28:19 +0530

    done

---
+++ portfolio/script.js
@@ -0,0 +1,43 @@
+
+/*const span = (text, index) => {
+    const node = document.createElement('span')
+  
+    node.textContent = text
+    node.style.setProperty('--index', index)
+    
+    return node
+  }
+  
+  const byLetter = text =>
+    [...text].map(span)
+  
+  const {matches:motionOK} = window.matchMedia(
+    '(prefers-reduced-motion: no-preference)'
+  )
+  
+  if (motionOK) {
+    const splitTargets = document.querySelectorAll('[split-by]')
+  
+    splitTargets.forEach(node => {
+      let nodes = byLetter(node.innerText)
+  
+      if (nodes)
+        node.firstChild.replaceWith(...nodes)
+    })
+
+
+ <img src="./images/stack/Next.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/Redux.svg" alt="" class="skills-logo" />
+ <img src="./images/stack/Bootstrap.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/MaterialUI.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/Express.png" alt="" class="skills-logo" />
+
+<img src="./images/stack/Graphql.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/MongoDB.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/Vercel.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/ChartJs.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/Bash.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/Docker.svg" alt="" class="skills-logo" />
+            <img src="./images/stack/K8s.svg" alt="" class="skills-logo" />
+  }*/
+          
